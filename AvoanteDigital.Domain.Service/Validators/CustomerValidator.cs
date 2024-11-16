ac2e154aac236497cb80223b82148bbbb44eb4fd
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2024-11-15 21:13:54 -0300

    feat: implementation of new validations

--- AvoanteDigital.Domain.Service/Validators/CustomerValidator.cs
+++ AvoanteDigital.Domain.Service/Validators/CustomerValidator.cs
@@ -13,10 +13,13 @@ public class CustomerValidator : AbstractValidator<Customer>
         
         RuleFor(c => c.Email)
             .NotEmpty().WithMessage("Por favor, insira o e-mail")
-            .NotNull().WithMessage("Por favor, insira o e-mail");
+            .NotNull().WithMessage("Por favor, insira o e-mail")
+            .Matches(@"@").WithMessage("O e-mail deve conter o caractere '@'");
         
-        RuleFor(c => c.Email)
+        RuleFor(c => c.TelephoneNumber)
             .NotEmpty().WithMessage("Por favor, insira o telefone")
-            .NotNull().WithMessage("Por favor, insira o telefone");
+            .NotNull().WithMessage("Por favor, insira o telefone")
+            .Must(t => !t.Contains(" ")).WithMessage("O telefone não pode conter espaços em branco")
+            .Length(11).WithMessage("O número de telefone deve ter exatamente 11 caracteres");
     }
 }

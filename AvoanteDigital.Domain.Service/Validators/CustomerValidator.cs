9f1a56985d6ccb0c9c2663dcf3eac087bbdea142
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2024-11-16 00:48:35 -0300

    feat: adding validations

--- AvoanteDigital.Domain.Service/Validators/CustomerValidator.cs
+++ AvoanteDigital.Domain.Service/Validators/CustomerValidator.cs
@@ -9,12 +9,15 @@ public class CustomerValidator : AbstractValidator<Customer>
     {
         RuleFor(c => c.Name)
             .NotEmpty().WithMessage("Por favor, insira o nome")
-            .NotNull().WithMessage("Por favor, insira o nome");
+            .NotNull().WithMessage("Por favor, insira o nome")
+            .MaximumLength(100).WithMessage("O nome não pode ter mais que 100 caracteres");
         
         RuleFor(c => c.Email)
             .NotEmpty().WithMessage("Por favor, insira o e-mail")
             .NotNull().WithMessage("Por favor, insira o e-mail")
-            .Matches(@"@").WithMessage("O e-mail deve conter o caractere '@'");
+            .Matches(@"@").WithMessage("O e-mail deve conter o caractere '@'")
+            .Matches(@"\.").WithMessage("O e-mail deve conter o caractere '.'")
+            .MaximumLength(50).WithMessage("O e-mail não pode ter mais que 50 caracteres");
         
         RuleFor(c => c.TelephoneNumber)
             .NotEmpty().WithMessage("Por favor, insira o telefone")
